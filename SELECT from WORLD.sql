f8cabac9c7182420cf4e267c98d30e4d206751c8
happy-bonbon <happy.yux@gmail.com>
2022-06-30 00:33:14 +1000

    more stuffs

--- SELECT from WORLD.sql
+++ SELECT from WORLD.sql
@@ -33,3 +33,39 @@ select name,
 from world
 where area >= 3000000
     or population >= 250000000;
+-- 8.One or the other (but not both)
+select name,
+    population,
+    area
+from world
+where (
+        area > 3000000
+        and population <= 250000000
+    )
+    or (
+        area <= 3000000
+        and population > 250000000
+    );
+-- 9. Rounding
+select name,
+    round(population / 1000000, 2),
+    round(gdp / 1000000000, 2)
+from world
+where continent = 'South America';
+-- 10. Trillion dollar economies
+select name,
+    round(gdp / population, -3)
+from world
+where gdp >= 1000000000000;
+-- 11. Name and capital have the same length
+SELECT name,
+    capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital);
+-- 12. Matching name and capital
+SELECT name,
+    capital
+FROM world
+where LEFT(name, 1) = LEFT(capital, 1)
+    and name <> capital;
+ -- 13. All the vowels
